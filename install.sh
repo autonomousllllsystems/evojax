132c1602d0ff7990f1239fe82d721d458b4e6105
Peter Lorenz <peter.lorenz@ntu.edu.sg>
2025-07-11 14:57:28 +0800

    update install

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+conda activate sd2 # installation in part2
+pip install evojax pyyaml
+# https://docs.jax.dev/en/latest/installation.html
+pip install -U "jax[cuda12]"
+# pip install -U "jax[tpu]"
+
+
+# pip install "jax[cuda11_pip]==0.4.23" optax -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
